6a3e05d2228ecde6e768f5b0212c4121f5fd491e
jonas <jonas.kazlauskas@me.com>
2021-04-17 16:35:30 -0700

    Add support for task logging

--- pipeline/utils.py
+++ pipeline/utils.py
@@ -3,14 +3,19 @@ def parse_line(line):
     Incoming data format for logging sensor readings
     ph: post: 7.4
     ec: post: 600
+    For task logs
+    task: post: 6000: ec
+    task: post: 4000: ph
     """
     data_list = line.split(":")
-    if len(data_list) == 3:
-        data = {"data":""}
-        data_list = line.split(":")
+    data = {"data":""}
+    if len(data_list) in [3, 4]:
         data["type"] = data_list.pop(0).strip().lower()
         data["request"] = data_list.pop(0).strip().lower()
-        for item in data_list:
-            data["data"] += item.strip().lower()
-        return data
-    return None
+        if len(data_list) == 4:
+            data["task_type"] = data_list.pop(1).strip.lower()
+    else:
+        return None
+    for item in data_list:
+        data["data"] += item.strip().lower()
+    return data
